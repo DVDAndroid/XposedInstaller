90b294b72d57f789b6f3ce8bc9a1785eacc38d6a
dvdandroid <davide.intelligent@gmail.com>
2017-03-17 15:52:24 +0100

    Some tweaks to notification for APK installing with root
    - Small code format

--- app/src/main/java/de/robv/android/xposed/installer/util/InstallApkUtil.java
+++ app/src/main/java/de/robv/android/xposed/installer/util/InstallApkUtil.java
@@ -12,15 +12,19 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import de.robv.android.xposed.installer.R;
 import de.robv.android.xposed.installer.XposedApp;
 
 public class InstallApkUtil extends AsyncTask<Void, Void, Integer> {
 
+    private static final int ERROR_ROOT_NOT_GRANTED = -99;
+
     private final DownloadsUtil.DownloadInfo info;
     private final Context context;
     private RootUtil mRootUtil;
-    private boolean enabled;
-    private List<String> output = new LinkedList<String>();
+    private boolean isApkRootInstallOn;
+    private List<String> output = new LinkedList<>();
+
     public InstallApkUtil(Context context, DownloadsUtil.DownloadInfo info) {
         this.context = context;
         this.info = info;
@@ -28,23 +32,36 @@ public class InstallApkUtil extends AsyncTask<Void, Void, Integer> {
         mRootUtil = new RootUtil();
     }
 
+    static void installApkNormally(Context context, String localFilename) {
+        Intent installIntent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+        installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        installIntent.setDataAndType(Uri.fromFile(new File(localFilename)), DownloadsUtil.MIME_TYPE_APK);
+        installIntent.putExtra(Intent.EXTRA_INSTALLER_PACKAGE_NAME, context.getApplicationInfo().packageName);
+        context.startActivity(installIntent);
+    }
+
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
 
         SharedPreferences prefs = XposedApp.getPreferences();
-        enabled = prefs.getBoolean("install_with_su", false);
+        isApkRootInstallOn = prefs.getBoolean("install_with_su", false);
 
-        if (enabled)
+        if (isApkRootInstallOn) {
             NotificationUtil.showModuleInstallingNotification(info.title);
             mRootUtil.startShell();
+        }
     }
 
     @Override
     protected Integer doInBackground(Void... params) {
         int returnCode = 0;
-        if (enabled) {
-            returnCode = mRootUtil.execute("pm install -r \"" + info.localFilename + "\"", output);
+        if (isApkRootInstallOn) {
+            try {
+                returnCode = mRootUtil.execute("pm install -r \"" + info.localFilename + "\"", output);
+            } catch (IllegalStateException e) {
+                returnCode = ERROR_ROOT_NOT_GRANTED;
+            }
         }
         return returnCode;
     }
@@ -53,8 +70,14 @@ public class InstallApkUtil extends AsyncTask<Void, Void, Integer> {
     protected void onPostExecute(Integer result) {
         super.onPostExecute(result);
 
-        if (enabled) {
+        if (isApkRootInstallOn) {
             NotificationUtil.cancel(NotificationUtil.NOTIFICATION_MODULE_INSTALLING);
+
+            if (result.equals(ERROR_ROOT_NOT_GRANTED)) {
+                NotificationUtil.showModuleInstallNotification(R.string.installation_error, R.string.root_failed, info.localFilename);
+                return;
+            }
+
             StringBuilder out = new StringBuilder();
             for (Object o : output) {
                 out.append(o.toString());
@@ -65,23 +88,12 @@ public class InstallApkUtil extends AsyncTask<Void, Void, Integer> {
             Matcher failureMatcher = failurePattern.matcher(out);
 
             if (result.equals(0)) {
-                String title = "Installation Successful";
-                String text = info.title + " installed successfully";
-                NotificationUtil.showModuleInstallNotification(title, text);
+                NotificationUtil.showModuleInstallNotification(R.string.installation_successful, R.string.installation_successful_message, info.localFilename, info.title);
             } else if (failureMatcher.find()) {
-                String reason =  failureMatcher.group(1);
-                String title = "Installation Failed";
-                String text = info.title + " installation failed (" + reason + ")";
-                NotificationUtil.showModuleInstallNotification(title, text);
+                NotificationUtil.showModuleInstallNotification(R.string.installation_error, R.string.installation_error_message, info.localFilename, failureMatcher.group(1));
             }
-        }
-
-        if (!enabled) {
-            Intent installIntent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
-            installIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            installIntent.setDataAndType(Uri.fromFile(new File(info.localFilename)), DownloadsUtil.MIME_TYPE_APK);
-            installIntent.putExtra(Intent.EXTRA_INSTALLER_PACKAGE_NAME, context.getApplicationInfo().packageName);
-            context.startActivity(installIntent);
+        } else {
+            installApkNormally(context, info.localFilename);
         }
     }
 }
